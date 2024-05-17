00f21b690b6c53471f906a50db54c8b3436d14e8
CARLOS CANDIDO <carlos.candido01@etec.sp.gov.br>
2024-05-17 13:41:49 -0300

    criação do controller do cadastro

---
+++ TCC-SIA/Controller/controleCadastro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCC_SIA.Controller
+{
+    internal class controleCadastro
+    {
+
+    }
+}
