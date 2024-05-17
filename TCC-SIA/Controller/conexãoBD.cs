536ce769ed92faf503b857796d41367b3c8f5e42
GUSTAVO PORTO <gustavo.porto01@etec.sp.gov.br>
2024-05-17 13:32:26 -0300

    Conexão bd npgsql +/-

--- TCC-SIA/Controller/conexãoBD.cs
+++ TCC-SIA/Controller/conexãoBD.cs
@@ -8,6 +8,53 @@ namespace TCC_SIA.Controller
 {
     internal class conexãoBD
     {
+        static string server = "127.0.0.1";
+        static string porta = "5433";
+        static string user = "postgres";
+        static string password = "2204";
+        static string dataBase = "Produto";
 
+        //objetos necessários para comunicação
+        NpgsqlConnection conn = null;
+
+        string connString = "Server=" + server + ";Port=" + porta + ";UserID=" + user + ";password=" + password + ";dataBase=" + dataBase + ";";
+
+        //metodo para conectar no banco de dados
+        public NpgsqlConnection conectar()
+        {
+            conn = new NpgsqlConnection(connString);
+            try
+            {
+                //abre a conexão com o banco de dados
+                conn.Open();
+
+                //retornar o status da conexão
+                return conn;
+            }
+            catch (NpgsqlException ex)//executa se a conexão der erro
+            {
+                //a variavel ex armazena o erro de conexão com o banco
+                return null;
+            }
+        }
+
+        //metodo para desconectar o banco de dados (só muda open para close)
+        public NpgsqlConnection desconectar()
+        {
+            conn = new NpgsqlConnection(connString);
+            try
+            {
+                //abre a conexão com o banco de dados
+                conn.Close();
+
+                //retornar o status da conexão
+                return conn;
+            }
+            catch (NpgsqlException ex)//executa se a conexão der erro
+            {
+                //a variavel ex armazena o erro de conexão com o banco
+                return null;
+            }
+        }
     }
 }
