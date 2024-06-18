efca66f407e15f06fe39fec000d858480373785f
GUSTAVO PORTO <gustavo.porto01@etec.sp.gov.br>
2024-06-18 15:10:04 -0300

    cadastro de veiculo

---
+++ TCC-SIA/Controller/controleVeiculo.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC_SIA.Model;
+using Npgsql;
+
+
+namespace TCC_SIA.Controller
+{
+    internal class controleVeiculo
+    {
+        public string cadastroVeiculo(Veiculo mVeiculo)
+        {
+            string sql = "insert into Veiculo(cpfcliente, corveiculo, marcaveiculo, tipoveiculo, modeloveiculo, placaveiculo) " + "values(@cpfcliente, @corveiculo, @marcaveiculo, @tipoveiculo, @modeloveiculo, @placaveiculo);";
+
+            conexãoBD con = new conexãoBD();
+            NpgsqlConnection conn = con.conectar();
+            NpgsqlCommand comm = new NpgsqlCommand(sql, conn);
+
+            try
+            {
+                comm.Parameters.AddWithValue("@cpfveiculo", mVeiculo.getCpfCliente());
+                comm.Parameters.AddWithValue("@corveiculo", mVeiculo.getCor());
+                comm.Parameters.AddWithValue("@marcaveiculo", mVeiculo.getMarca());
+                comm.Parameters.AddWithValue("@tipoveiculo", mVeiculo.getTipo());
+                comm.Parameters.AddWithValue("@modeloveiculo", mVeiculo.getModelo());
+                comm.Parameters.AddWithValue("@placaveiculo", mVeiculo.getPlaca());
+
+                comm.ExecuteNonQuery();
+                return "Veículo cadastrado com sucesso!";
+            }
+            catch (NpgsqlException ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ TCC-SIA/Model/Veiculo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCC_SIA.Model
+{
+    internal class Veiculo
+    {
+        private int cpfcliente;
+        private string cor;
+        private string marca;
+        private string tipo;
+        private string modelo;
+        private string placa;
+
+        public int getCpfCliente()
+        {
+            return this.cpfcliente;
+        }
+
+        public void setCpfCliente(int CpfCliente)
+        {
+            this.cpfcliente = CpfCliente;
+        }
+
+        public string getCor()
+        {
+            return this.cor;
+        }
+
+        public void setCor(string Cor)
+        {
+            this.cor = Cor;
+        }
+
+        public string getMarca()
+        {
+            return this.marca;
+        }
+
+        public void setMarca(string Marca)
+        {
+            this.marca = Marca;
+        }
+
+        public string getTipo()
+        {
+            return this.tipo;
+        }
+
+        public void setTipo(string Tipo)
+        {
+            this.tipo = Tipo;
+        }
+
+        public string getModelo()
+        {
+            return this.modelo;
+        }
+
+        public void setModelo(string Modelo)
+        {
+            this.modelo = Modelo;
+        }
+
+        public string getPlaca()
+        {
+            return this.placa;
+        }
+
+        public void setPlaca(string Placa)
+        {
+            this.placa = Placa;
+        }
+    }
+}
