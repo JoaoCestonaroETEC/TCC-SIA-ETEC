1cf62f7e4bffa0ab0fd9adbe27be65d0bac96590
JOAO CESTONARO <joao.cestonaro@etec.sp.gov.br>
2024-06-20 20:10:46 -0300

    fazendo o cadastro do servico

---
+++ TCC-SIA/Controller/controleServico.cs
@@ -0,0 +1,39 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC_SIA.Model;
+
+namespace TCC_SIA.Controller
+{
+    internal class controleServico
+    {
+        public string cadastroServico(Servico mServico)
+        {
+            string sql = "INSERT INTO SERVICO(NOMESERVICO, TIPOSERVICO, DESCSERVICO, VALORSERVICO) " + "values(@NOMESERVICO, @TIPOSERVICO, @DESCSERVICO, @VALORSERVICO);";
+
+            conexãoBD con = new conexãoBD();
+            NpgsqlConnection conn = con.conectar();
+            NpgsqlCommand comm = new NpgsqlCommand(sql, conn);
+
+            try
+            {
+                comm.Parameters.AddWithValue("@NOMEPECA", mPeca.getNomePeca());
+                comm.Parameters.AddWithValue("@TIPOPECA", mPeca.getTipoPeca());
+                comm.Parameters.AddWithValue("@DESCPECA", mPeca.getDescPeca());
+                comm.Parameters.AddWithValue("@VALORPECA", mPeca.getValorPeca());
+                comm.Parameters.AddWithValue("@QUANTPECA", mPeca.getQuantPeca());
+                comm.Parameters.AddWithValue("@MARCAPECA", mPeca.getMarcaPeca());
+
+                comm.ExecuteNonQuery();
+                return "Veículo cadastrado com sucesso!";
+            }
+            catch (NpgsqlException ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ TCC-SIA/Model/Servico.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCC_SIA.Model
+{
+    internal class Servico
+    {
+        private string nomeServico;
+        private string descServico;
+        private string tipoServico;
+        private long valorServico;
+
+        public string NomeServico
+        {
+            get { return nomeServico; }
+            set { NomeServico = value; }
+        }
+
+        public string DescServico
+        {
+            get { return descServico; }
+            set { DescServico = value; }
+        }
+
+        public string TipoServico
+        {
+            get { return tipoServico; }
+            set { tipoServico = value; }
+        }
+
+        public long ValorServico
+        {
+            get { return valorServico; }
+            set { ValorServico = value; }
+        }
+    }
+}
