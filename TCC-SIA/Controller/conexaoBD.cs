145edc51a0ea88bc4285911b6ce4638d946e9079
GUSTAVO PORTO <gustavo.porto01@etec.sp.gov.br>
2024-06-20 15:09:59 -0300

    Model/controler

---
+++ TCC-SIA/Controller/conexaoBD.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace TCC_SIA.Controller
+{
+    internal class conexaoBD
+    {
+        static string server = "127.0.0.1";
+        static string porta = "5432";
+        static string user = "postgres";
+        static string password = "123456";
+        static string dataBase = "TCC-SIA";
+
+
+        //objetos necessários para comunicação
+        NpgsqlConnection conn = null;
+
+        string connString = "Server=" + server + ";Port=" + porta + ";UserID=" + user + ";password=" + password + ";dataBase=" + dataBase + ";";
+
+        //metodo para conectar no banco de dados
+        public NpgsqlConnection conectar()
+        {
+            conn = new NpgsqlConnection(connString);
+            try
+            {
+                //abre a conexão com o banco de dados
+                conn.Open();
+
+                //retornar o status da conexão
+                return conn;
+            }
+            catch (NpgsqlException ex)//executa se a conexão der erro
+            {
+                //a variavel ex armazena o erro de conexão com o banco
+                return null;
+            }
+        }
+
+        //metodo para desconectar o banco de dados (só muda open para close)
+        public NpgsqlConnection desconectar()
+        {
+            conn = new NpgsqlConnection(connString);
+            try
+            {
+                //abre a conexão com o banco de dados
+                conn.Close();
+
+                //retornar o status da conexão
+                return conn;
+            }
+            catch (NpgsqlException ex)//executa se a conexão der erro
+            {
+                //a variavel ex armazena o erro de conexão com o banco
+                return null;
+            }
+        }
+    }
+}
---
+++ TCC-SIA/View/CadastroServiço.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace TCC_SIA.View
+{
+    partial class CadastroServiço
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            comboBox1 = new ComboBox();
+            maskedTextBox2 = new MaskedTextBox();
+            comboBox2 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            label7 = new Label();
+            comboBox3 = new ComboBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(47, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(55, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Tipo:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(51, 117);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Valor:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(499, 50);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Descrição:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(40, 150);
+            label5.Name = "label5";
+            label5.Size = new Size(60, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Veículo:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(53, 186);
+            label6.Name = "label6";
+            label6.Size = new Size(44, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Data:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(103, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(340, 27);
+            textBox1.TabIndex = 6;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(579, 47);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(385, 269);
+            richTextBox1.TabIndex = 7;
+            richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(103, 80);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(340, 28);
+            comboBox1.TabIndex = 8;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(103, 114);
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(340, 27);
+            maskedTextBox2.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(103, 147);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(340, 28);
+            comboBox2.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(103, 181);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(340, 27);
+            dateTimePicker1.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(55, 217);
+            label7.Name = "label7";
+            label7.Size = new Size(42, 20);
+            label7.TabIndex = 12;
+            label7.Text = "Peça:";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(103, 214);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(340, 28);
+            comboBox3.TabIndex = 13;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(47, 267);
+            button1.Name = "button1";
+            button1.Size = new Size(186, 149);
+            button1.TabIndex = 14;
+            button1.Text = "Cadastrar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // CadastroServiço
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(996, 542);
+            Controls.Add(button1);
+            Controls.Add(comboBox3);
+            Controls.Add(label7);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox2);
+            Controls.Add(maskedTextBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "CadastroServiço";
+            Text = "CadastroServiço";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private ComboBox comboBox1;
+        private MaskedTextBox maskedTextBox2;
+        private ComboBox comboBox2;
+        private DateTimePicker dateTimePicker1;
+        private Label label7;
+        private ComboBox comboBox3;
+        private Button button1;
+    }
+}
