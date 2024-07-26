93b9e535dcae829369805f9da248db4d375adb61
CARLOS CANDIDO <carlos.candido01@etec.sp.gov.br>
2024-07-26 13:17:41 -0300

    Tentativa do atualizar

--- TCC-SIA/Controller/ControleCliente.cs
+++ TCC-SIA/Controller/ControleCliente.cs
@@ -138,7 +138,47 @@ namespace TCC_SIA.Controller
                 con.desconectar();
             }
 
+            #endregion
+
+            
+
         }
-        #endregion
+        public string atualizaCliente(Cliente cliente)
+        {
+            string sql = "SELECT C.CPFCLIENTE, C.NOMECLIENTE, C.EMAILCLIENTE, C.DATANASC_CLIENTE, C.SEXO, " +
+                         "E.NUMERO, E.RUA, E.CIDADE, E.CEP, E.BAIRRO, E.ESTADO, T.TELEFONE " +
+                         "FROM CLIENTE C " +
+                         "INNER JOIN CLIENTE_ENDERECO E ON C.CPFCLIENTE = E.CPFCLIENTE " +
+                         "INNER JOIN CLIENTE_TELEFONE T ON C.CPFCLIENTE = T.CPFCLIENTE " +
+                         "WHERE C.NOMECLIENTE LIKE '" + cliente + "%';";
+
+            conexaoBD con = new conexaoBD();
+            NpgsqlConnection conn = con.conectar();
+            NpgsqlCommand comm = new NpgsqlCommand(sql, conn);
+
+            //Fazendo o try
+            try
+            {
+                comm.Parameters.AddWithValue("@nomecliente", Cliente.getNomeCliente());
+
+
+                comm.ExecuteNonQuery();
+                return "Produto atualizado!";
+            }
+            //Fazendo o catch
+            catch (NpgsqlException ex)
+            {
+                //Retornando como nulo
+                return null;
+            }
+            //Encerrando a conexão
+            finally
+            {
+                //Método de desconectar
+                con.desconectar();
+            }
+
+
+        }
     }
 }
