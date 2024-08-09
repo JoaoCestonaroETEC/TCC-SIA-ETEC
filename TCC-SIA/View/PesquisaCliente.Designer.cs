fb26ba762ab44f82a3264db5af73e6ef1b6ebe22
CARLOS CANDIDO <carlos.candido01@etec.sp.gov.br>
2024-08-09 12:45:09 -0300

    Comeco

--- TCC-SIA/View/PesquisaCliente.Designer.cs
+++ TCC-SIA/View/PesquisaCliente.Designer.cs
@@ -28,68 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridViewPesquisar = new DataGridView();
-            textBoxPesquisar = new TextBox();
-            label1 = new Label();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
             buttonPesquisar = new Button();
+            label1 = new Label();
+            textBoxPesquisar = new TextBox();
+            dataGridViewPesquisar = new DataGridView();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridViewPesquisar).BeginInit();
             SuspendLayout();
             // 
-            // dataGridViewPesquisar
+            // tabControl1
             // 
-            dataGridViewPesquisar.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewPesquisar.Location = new Point(12, 43);
-            dataGridViewPesquisar.Name = "dataGridViewPesquisar";
-            dataGridViewPesquisar.Size = new Size(514, 295);
-            dataGridViewPesquisar.TabIndex = 0;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(591, 545);
+            tabControl1.TabIndex = 0;
             // 
-            // textBoxPesquisar
+            // tabPage1
             // 
-            textBoxPesquisar.Location = new Point(78, 12);
-            textBoxPesquisar.Name = "textBoxPesquisar";
-            textBoxPesquisar.Size = new Size(448, 23);
-            textBoxPesquisar.TabIndex = 1;
+            tabPage1.Controls.Add(buttonPesquisar);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(textBoxPesquisar);
+            tabPage1.Controls.Add(dataGridViewPesquisar);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(583, 512);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(242, 92);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // buttonPesquisar
+            // 
+            buttonPesquisar.Location = new Point(0, 452);
+            buttonPesquisar.Margin = new Padding(3, 4, 3, 4);
+            buttonPesquisar.Name = "buttonPesquisar";
+            buttonPesquisar.Size = new Size(136, 95);
+            buttonPesquisar.TabIndex = 7;
+            buttonPesquisar.Text = "Pesquisar";
+            buttonPesquisar.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 15);
+            label1.Location = new Point(0, 13);
             label1.Name = "label1";
-            label1.Size = new Size(60, 15);
-            label1.TabIndex = 2;
+            label1.Size = new Size(73, 20);
+            label1.TabIndex = 6;
             label1.Text = "Pesquisar:";
             // 
-            // buttonPesquisar
+            // textBoxPesquisar
             // 
-            buttonPesquisar.Location = new Point(12, 344);
-            buttonPesquisar.Name = "buttonPesquisar";
-            buttonPesquisar.Size = new Size(119, 71);
-            buttonPesquisar.TabIndex = 3;
-            buttonPesquisar.Text = "Pesquisar";
-            buttonPesquisar.UseVisualStyleBackColor = true;
-            buttonPesquisar.Click += buttonPesquisar_Click;
+            textBoxPesquisar.Location = new Point(75, 9);
+            textBoxPesquisar.Margin = new Padding(3, 4, 3, 4);
+            textBoxPesquisar.Name = "textBoxPesquisar";
+            textBoxPesquisar.Size = new Size(511, 27);
+            textBoxPesquisar.TabIndex = 5;
+            // 
+            // dataGridViewPesquisar
+            // 
+            dataGridViewPesquisar.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewPesquisar.Location = new Point(0, 50);
+            dataGridViewPesquisar.Margin = new Padding(3, 4, 3, 4);
+            dataGridViewPesquisar.Name = "dataGridViewPesquisar";
+            dataGridViewPesquisar.RowHeadersWidth = 51;
+            dataGridViewPesquisar.Size = new Size(587, 393);
+            dataGridViewPesquisar.TabIndex = 4;
             // 
             // PesquisaCliente
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(538, 427);
-            Controls.Add(buttonPesquisar);
-            Controls.Add(label1);
-            Controls.Add(textBoxPesquisar);
-            Controls.Add(dataGridViewPesquisar);
+            ClientSize = new Size(615, 569);
+            Controls.Add(tabControl1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "PesquisaCliente";
             Text = "PesquisarCliente";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridViewPesquisar).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private DataGridView dataGridViewPesquisar;
-        private TextBox textBoxPesquisar;
-        private Label label1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
         private Button buttonPesquisar;
+        private Label label1;
+        private TextBox textBoxPesquisar;
+        private DataGridView dataGridViewPesquisar;
+        private TabPage tabPage2;
     }
 }
